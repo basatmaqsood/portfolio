7009135d14e32ef3d87f61d0e0adddb4cce4d721
basat maqsood <itzbasatmaqsood@gmail.com>
2024-05-30 01:02:01 +0500

    Portfolio Completed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/basatmaqsood'
+      className='link footer__link'
+    >
+      With ❤️ By Basat Maqsood
+    </a>
+  </footer>
+)
+
+export default Footer
